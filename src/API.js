32845b176b82d36a881a1cb12684f89933a9e348
Apoorva Bhaisare <99715304+Apoorvasmr@users.noreply.github.com>
2022-12-28 19:10:22 +0530

    Update API.js

--- src/API.js
+++ src/API.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 // } else {
 //     baseURL = 'http://127.0.0.1:8000';
 // }
-// baseURL = 'http://127.0.0.1:8000';
+baseURL = 'http://127.0.0.1:8000';
 const api = axios.create({
     baseURL: baseURL,
     headers: {
